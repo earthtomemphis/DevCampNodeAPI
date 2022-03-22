7e7758ce81f31858fd027b09aa171062ca8ae68c
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-03-22 00:37:47 -0600

    mongo setup and colors

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcampRoutes = require('./routes/bootcampRoutes');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcampRoutes = require('./routes/bootcampRoutes');
 
 const app = express();
 
@@ -20,9 +25,17 @@ app.use('/api/v1/bootcamps', bootcampRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+			.bold
 	)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	// Close server and exit process
+	server.close(() => process.exit(1));
+});
